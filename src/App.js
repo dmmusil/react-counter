d177cb60dcd6a8f8d58672be6dc890c533e9128b
Dylan Musil <dmusil@degreed.com>
2019-12-14 15:40:04 -0500

    Counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    player: 0,
+    players: [
+      {
+        name: "Kate",
+        score: 0
+      },
+      {
+        name: "Dylan",
+        score: 0
+      }
+    ]
+  };
+
+  handleClick() {
+    // copy the state
+    const newState = { ...this.state };
+    // save a referece to the players
+    const players = newState.players;
+    // get the current player
+    const currentPlayer = newState.player;
+    // update the current player's score
+    players[currentPlayer].score++;
+    // set the current player to the other player
+    newState.player = (newState.player + 1) % 2;
+
+    // this is a fancy way of doing this
+    // newState.player = newState.player + 1;
+    // if (newState.player > 1) newState.player = 0;
+    // the newState.player is used to find the player in the player array so you know which score to update
+
+    this.setState(newState);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>{this.state.players[0].name}</div>
+        <div>{this.state.players[0].score}</div>
+        <button onClick={() => this.handleClick()}>Go</button>
+        <div>{this.state.players[1].name}</div>
+        <div>{this.state.players[1].score}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
